5ed72c859e2c3f05d39cf27a8dcb503f7ad27ddc
MIYABETaisei <mytspe46@gmail.com>
2023-01-21 21:00:36 +0900

    add: Headerコンポーネント作成

---
+++ src/layout/Header.tsx
@@ -0,0 +1,49 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
+import type {FC} from 'react'
+
+/**
+ * @package
+ */
+export const Header: FC = () => {
+  return (
+    <header css={section}>
+      <div css={container}>
+        <h1 css={title}>
+          YUMEMI
+          <br />
+          <span>都道府県別の総人口推移グラフ</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
+
+const section = css`
+  width: 100%;
+  ::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 8px;
+    background: linear-gradient(rgba(65, 164, 253, 1), transparent);
+  }
+`
+const container = css`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+`
+const title = css`
+  font-size: 36px;
+  font-weight: bold;
+  text-align: center;
+  letter-spacing: 0.1em;
+  line-height: 1.1;
+  padding: 25px 0;
+  span {
+    font-size: 22px;
+    padding-top: 5px;
+  }
+`
