93a43bd01dab486402f92bc39855abc72a586bbe
MIYABETaisei <mytspe46@gmail.com>
2023-01-21 21:01:04 +0900

    add: Footerコンポーネント作成

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
+import type {FC} from 'react'
+
+/**
+ * @package
+ */
+export const Footer: FC = () => {
+  return (
+    <footer css={section}>
+      <small css={copyright}>© 2023 Taisei Miyabe.</small>
+    </footer>
+  )
+}
+
+const section = css`
+  width: 100%;
+  padding: 20px;
+  text-align: center;
+`
+const copyright = css`
+  font-size: 12px;
+`
