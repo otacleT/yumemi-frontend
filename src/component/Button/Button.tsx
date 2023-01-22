db77446103329d52fa848244998ae57593979afd
MIYABETaisei <mytspe46@gmail.com>
2023-01-22 10:49:28 +0900

    add: 都道府県のチェックボックスコンポーネント作成

---
+++ src/component/Button/Button.tsx
@@ -0,0 +1,60 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
+import type {FC} from 'react'
+import {useState} from 'react'
+
+type Prefecture = {
+  name: string
+}
+
+/**
+ * @package
+ */
+export const Button: FC<Prefecture> = ({name}) => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  return (
+    <label css={label(isChecked)}>
+      <input
+        css={check}
+        type='checkbox'
+        checked={isChecked}
+        onChange={() => {
+          return setIsChecked(!isChecked)
+        }}
+      />
+      {name}
+    </label>
+  )
+}
+
+const label = (isChecked: boolean) => {
+  return [
+    css`
+      font-size: 15px;
+      font-weight: bold;
+      display: flex;
+      width: 110px;
+      height: 36px;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      color: #000;
+      border-radius: 25px;
+      ${isChecked ? 'border: 3px solid rgba(65, 164, 253, 1);' : 'border: 3px solid #d9d9d9;'}
+      position: relative;
+      cursor: pointer;
+      ::before {
+        font-size: 15px;
+        font-weight: bold;
+        content: ${isChecked ? '"✓"' : '"＋"'};
+        display: inline-block;
+        padding-right: 10px;
+        color: ${isChecked ? 'rgba(65, 164, 253, 1)' : '#000'};
+      }
+    `,
+  ]
+}
+const check = css`
+  display: none;
+`
