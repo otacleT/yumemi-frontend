e251a120484e23fdcdd1acd3b1917e572714a8b2
MIYABETaisei <mytspe46@gmail.com>
2023-01-22 11:10:19 +0900

    add: 取得した都道府県一覧よりチェックボックスを生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
 import type {GetStaticProps, NextPage} from 'next'
 
+import {Button} from '@/component/Button'
 import type {Prefecture} from '@/type/Prefecture'
 import type {Prefectures} from '@/type/Prefectures'
 
@@ -26,8 +29,46 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const Home: NextPage<Prefectures> = (props) => {
   const {prefectures} = props
-  console.info(prefectures)
-  return <div></div>
+  return (
+    <div css={[container, wrapper]}>
+      <div>
+        <h2 css={title}>都道府県一覧</h2>
+        <div css={leftWrapper}>
+          {prefectures.map(({prefCode, prefName}: Prefecture) => {
+            return <Button key={prefCode} name={prefName} />
+          })}
+        </div>
+      </div>
+      <div></div>
+    </div>
+  )
 }
 
 export default Home
+
+const container = css`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 60px 20px;
+`
+const wrapper = css`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: minmax(340px, 2fr) 3fr;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+const title = css`
+  font-size: 20px;
+  font-weight: bold;
+`
+const leftWrapper = css`
+  width: 100%;
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+  place-items: center;
+  padding-top: 20px;
+`
