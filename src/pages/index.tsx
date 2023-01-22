7346b43e9db3256079ddf978fc3540d944fa4fbc
MIYABETaisei <mytspe46@gmail.com>
2023-01-22 11:00:34 +0900

    add: getStaticPropsを使って都道府県一覧を静的に取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
-import type {NextPage} from 'next'
+import type {GetStaticProps, NextPage} from 'next'
 
-const Home: NextPage = () => {
+import type {Prefecture} from '@/type/Prefecture'
+import type {Prefectures} from '@/type/Prefectures'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const requestHeaders: HeadersInit = new Headers()
+  requestHeaders.append('X-API-KEY', process.env.RESAS_API_KEY ?? '')
+  const res = await fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+    method: 'GET',
+    headers: requestHeaders,
+  })
+  const data = await res.json()
+  const prefectures = data.result.map((prefecture: Prefecture) => {
+    return {
+      prefCode: prefecture.prefCode,
+      prefName: prefecture.prefName,
+    }
+  })
+  return {
+    props: {
+      prefectures,
+    },
+  }
+}
+
+const Home: NextPage<Prefectures> = (props) => {
+  const {prefectures} = props
+  console.info(prefectures)
   return <div></div>
 }
 
