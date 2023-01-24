33730c158018ded3262fbf45bf6f0dbc872e44d8
MIYABETaisei <mytspe46@gmail.com>
2023-01-24 11:37:28 +0900

    add: Chartコンポーネント名作成 #2

---
+++ src/component/Chart/Chart.tsx
@@ -0,0 +1,64 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import type {FC} from 'react'
+import {useMemo} from 'react'
+
+import type {ChartData} from '@/type/ChartData'
+
+type Props = {
+  chartData: ChartData[]
+}
+
+/**
+ * @package
+ */
+export const Chart: FC<Props> = (props) => {
+  const {chartData} = props
+  const options = useMemo(() => {
+    return {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: undefined,
+      },
+      xAxis: {
+        title: {
+          text: '年度',
+        },
+      },
+      yAxis: {
+        title: {
+          text: '人口数',
+        },
+      },
+      plotOptions: {
+        column: {
+          pointStart: 100000,
+          pointInterval: 100000,
+        },
+        line: {
+          pointInterval: 5,
+          pointStart: 1980,
+        },
+      },
+      series: chartData.map((data: ChartData) => {
+        return {
+          name: data.name,
+          data: [...data.data],
+        }
+      }),
+    }
+  }, [chartData])
+  return (
+    <div css={chart}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  )
+}
+
+const chart = css`
+  width: 100%;
+`
