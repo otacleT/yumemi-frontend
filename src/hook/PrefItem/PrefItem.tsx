f28d90df1dc82ced8855d2d53a489c224992d3cf
MIYABETaisei <mytspe46@gmail.com>
2023-01-24 12:15:35 +0900

    clean: 都道府県選択時に実行する関数をカスタムフックとして分ける #6

---
+++ src/hook/PrefItem/PrefItem.tsx
@@ -0,0 +1,51 @@
+import type {Dispatch, SetStateAction} from 'react'
+import {useCallback, useState} from 'react'
+
+import type {ChartData} from '@/type/ChartData'
+
+type Data = {
+  year: number
+  value: number
+}
+
+/**
+ * @package
+ */
+export const usePrefItem = () => {
+  const [isChecked, setIsChecked] = useState(false)
+  const handleChange = useCallback(
+    async (
+      prefCode: number,
+      prefName: string,
+      setChartData: Dispatch<SetStateAction<ChartData[]>>
+    ) => {
+      if (isChecked) {
+        setChartData((prev) => {
+          return prev.filter((item) => {
+            return item.name !== prefName
+          })
+        })
+      } else {
+        const requestHeaders: HeadersInit = new Headers()
+        requestHeaders.append('text', prefCode.toString())
+        const res = await fetch('/api/population', {
+          method: 'GET',
+          headers: requestHeaders,
+        })
+        const data = await res.json()
+        const population = data.result.data[0].data.map((item: Data) => {
+          return item.value
+        })
+        setChartData((prev) => {
+          return [...prev, {name: prefName, data: population}]
+        })
+      }
+      setIsChecked((prev) => {
+        return !prev
+      })
+    },
+    [isChecked]
+  )
+
+  return {isChecked, handleChange}
+}
