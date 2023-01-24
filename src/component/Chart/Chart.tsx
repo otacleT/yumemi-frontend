5d00e40c32f54935ae363eeddd0b16e11744add2
MIYABETaisei <mytspe46@gmail.com>
2023-01-24 21:54:09 +0900

    add: Chartに渡す配列の長さが0のときの処理を追加 #8

--- src/component/Chart/Chart.tsx
+++ src/component/Chart/Chart.tsx
@@ -65,11 +65,37 @@ export const Chart: FC<Props> = (props) => {
   }, [chartData])
   return (
     <div css={chart}>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <div css={chart}>
+        {chartData.length === 0 ? (
+          <div css={noData}>
+            <p>都道府県を選択してください</p>
+          </div>
+        ) : (
+          <HighchartsReact highcharts={Highcharts} options={options} />
+        )}
+      </div>
     </div>
   )
 }
 
 const chart = css`
   width: 100%;
+`
+const noData = css`
+  width: 100%;
+  height: 0;
+  padding-top: 65%;
+  background-image: linear-gradient(90deg, #41a4fd, #677efa);
+  border-radius: 10px;
+  position: relative;
+  p {
+    font-size: 24px;
+    font-weight: 700;
+    color: #fff;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `
