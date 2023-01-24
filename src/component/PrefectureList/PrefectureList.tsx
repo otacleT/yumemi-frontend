04e2a48789714db21062135d1483265fc7950891
MIYABETaisei <mytspe46@gmail.com>
2023-01-24 11:38:57 +0900

    add: 都道府県一覧をコンポーネントにまとめる #2

---
+++ src/component/PrefectureList/PrefectureList.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react'
+import type {Dispatch, FC, SetStateAction} from 'react'
+
+import type {ChartData} from '@/type/ChartData'
+import type {Prefecture} from '@/type/Prefecture'
+
+import {PrefectureItem} from '../PrefectureItem'
+
+type Props = {
+  prefectures: Prefecture[]
+  setChartData: Dispatch<SetStateAction<ChartData[]>>
+}
+
+export const PrefectureList: FC<Props> = (props) => {
+  const {prefectures, setChartData} = props
+  return (
+    <div>
+      <h2 css={title}>都道府県一覧</h2>
+      <ul css={list}>
+        {prefectures.map(({prefCode, prefName}: Prefecture) => {
+          return (
+            <PrefectureItem
+              key={prefCode}
+              prefName={prefName}
+              prefCode={prefCode}
+              setChartData={setChartData}
+            />
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+const title = css`
+  font-size: 20px;
+  font-weight: bold;
+`
+const list = css`
+  width: 100%;
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+  place-items: center;
+  padding-top: 20px;
+`
