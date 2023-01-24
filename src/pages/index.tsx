51b0e46ba152eb9bd836b0d54e7e88459b2f3d04
MIYABETaisei <mytspe46@gmail.com>
2023-01-24 11:41:01 +0900

    clean: Chart,PrefectureListコンポーネントをインポート #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react'
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
 import type {GetStaticProps, NextPage} from 'next'
-import {useMemo, useState} from 'react'
+import {useState} from 'react'
 
-import {Button} from '@/component/Button'
+import {Chart} from '@/component/Chart'
+import {PrefectureList} from '@/component/PrefectureList'
 import type {ChartData} from '@/type/ChartData'
 import type {Prefecture} from '@/type/Prefecture'
 import type {Prefectures} from '@/type/Prefectures'
@@ -34,63 +33,12 @@ export const getStaticProps: GetStaticProps = async () => {
 const Home: NextPage<Prefectures> = (props) => {
   const {prefectures} = props
   const [chartData, setChartData] = useState<ChartData[]>([])
-  const options = useMemo(() => {
-    return {
-      chart: {
-        type: 'line',
-      },
-      title: {
-        text: undefined,
-      },
-      xAxis: {
-        title: {
-          text: '年度',
-        },
-      },
-      yAxis: {
-        title: {
-          text: '人口数',
-        },
-      },
-      plotOptions: {
-        column: {
-          pointStart: 100000,
-          pointInterval: 100000,
-        },
-        line: {
-          pointInterval: 5,
-          pointStart: 1980,
-        },
-      },
-      series: chartData.map((data: ChartData) => {
-        return {
-          name: data.name,
-          data: [...data.data],
-        }
-      }),
-    }
-  }, [chartData])
+
   return (
-    <div css={[container, wrapper]}>
-      <div>
-        <h2 css={title}>都道府県一覧</h2>
-        <div css={leftWrapper}>
-          {prefectures.map(({prefCode, prefName}: Prefecture) => {
-            return (
-              <Button
-                key={prefCode}
-                prefName={prefName}
-                prefCode={prefCode}
-                setChartData={setChartData}
-              />
-            )
-          })}
-        </div>
-      </div>
-      <div css={chart}>
-        <HighchartsReact highcharts={Highcharts} options={options} />
-      </div>
-    </div>
+    <section css={[container, wrapper]}>
+      <PrefectureList prefectures={prefectures} setChartData={setChartData} />
+      <Chart chartData={chartData} />
+    </section>
   )
 }
 
@@ -110,19 +58,4 @@ const wrapper = css`
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
-`
-const chart = css`
-  width: 100%;
-`
-const title = css`
-  font-size: 20px;
-  font-weight: bold;
-`
-const leftWrapper = css`
-  width: 100%;
-  display: grid;
-  gap: 5px;
-  grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
-  place-items: center;
-  padding-top: 20px;
 `
