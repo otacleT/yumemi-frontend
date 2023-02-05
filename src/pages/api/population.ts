8d714d5c4bfc2e142d861a4eb9891014345072f8
MIYABETaisei <mytspe46@gmail.com>
2023-02-05 10:06:34 +0900

    add: APIにエラー処理追加 #25

--- src/pages/api/population.ts
+++ src/pages/api/population.ts
@@ -4,14 +4,19 @@ export default async function resasApi(req: Req, res: Res) {
   const prefCode = req.headers['text'] as string
   const requestHeaders: HeadersInit = new Headers()
   requestHeaders.append('X-API-KEY', process.env.RESAS_API_KEY ?? '')
-  const response = await fetch(
+  const data = await fetch(
     `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=-`,
     {
       method: 'GET',
       headers: requestHeaders,
     }
   )
-  const data = await response.json()
+    .then((res) => {
+      return res.json()
+    })
+    .catch((err) => {
+      res.status(500).json({message: err.message})
+    })
 
   res.status(200).json(data)
 }
