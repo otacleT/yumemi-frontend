afadfad86a76c2595cabdfa60c317e6e772e2c6e
MIYABETaisei <mytspe46@gmail.com>
2023-08-22 11:54:08 +0900

    add: ✨ SSRで都道府県の一覧情報を取得する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,60 @@
-import type {NextPage} from 'next'
+import type {GetStaticProps, NextPage} from 'next'
 
 import {Home} from '@/component/Home'
+import type {PrefectureType} from '@/type/PrefectureType'
 
-const HomePage: NextPage = () => {
-  return <Home />
+type PrefecturesRes = {
+  message: string
+  result: PrefectureType[]
+}
+
+type HomePageProps = {
+  prefectures: PrefectureType[]
+  error?: string
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const res = await fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': process.env.RESAS_API_KEY || '',
+      },
+    })
+
+    if (!res.ok) {
+      throw new Error(`API response error with status code: ${res.status}`)
+    }
+
+    const data = (await res.json()) as PrefecturesRes
+    const prefectures = data.result.map((prefecture) => {
+      return {
+        prefCode: prefecture.prefCode,
+        prefName: prefecture.prefName,
+      }
+    })
+
+    return {
+      props: {
+        prefectures,
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        prefectures: [],
+        error: JSON.stringify(error),
+      },
+    }
+  }
+}
+
+const HomePage: NextPage<HomePageProps> = ({error, prefectures}) => {
+  console.info(error)
+  if (error) {
+    return <div>都道府県一覧の取得に失敗しました。</div>
+  }
+  return <Home prefectures={prefectures} />
 }
 
 export default HomePage
