b079abf314a32d67060563b05983b27d9446e5c0
MIYABETaisei <mytspe46@gmail.com>
2023-08-23 11:39:06 +0900

    add: ✨ next/fontを使ってnotoSansJPをフォントに設定 #3

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,15 @@
+import {Noto_Sans_JP} from 'next/font/google'
+
 import {Footer} from '@/layout/Footer'
 import {Header} from '@/layout/Header'
+
+const notoSansJP = Noto_Sans_JP({
+  weight: ['400', '700', '800'],
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-jp',
+})
 
 /**
  * @package
@@ -7,10 +17,10 @@ import {Header} from '@/layout/Header'
 
 export const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <>
+    <div className={`${notoSansJP.variable} font-sans`}>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
