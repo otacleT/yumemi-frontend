948bac44863dfc182fa2994da8f49344feea1c35
MIYABETaisei <mytspe46@gmail.com>
2023-08-23 22:26:55 +0900

    add: ✨ 検索エンジンにインデックスされないように HTTP ヘッダーを設定 #4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    const headers = []
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      })
+    }
+    return headers
+  },
 }
 
 module.exports = nextConfig
