06b1080a070bde4b81b345531f56afaeb6e9e7be
MIYABETaisei <mytspe46@gmail.com>
2023-08-23 16:54:02 +0900

    style: 💄 Headerのレスポンシブ対応 #5

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,9 +6,9 @@ import React from 'react'
 
 export const Header: React.FC = () => {
   return (
-    <header className='relative w-full from-[#41a4fd] py-10 text-center after:absolute after:bottom-0 after:left-0 after:block after:h-2 after:w-full after:translate-y-full after:bg-gradient-to-b after:content-[""]'>
-      <h1 className='text-4xl font-extrabold'>YUMEMI</h1>
-      <p className='mt-1 text-xl font-bold'>都道府県別の総人口推移グラフ</p>
+    <header className='relative w-full from-[#41a4fd] py-6 sm:py-10 text-center after:absolute after:bottom-0 after:left-0 after:block after:h-2 after:w-full after:translate-y-full after:bg-gradient-to-b after:content-[""]'>
+      <h1 className='md:text-4xl font-extrabold sm:text-3xl text-2xl'>YUMEMI</h1>
+      <p className='mt-1 md:text-xl font-bold sm:text-lg text-base'>都道府県別の総人口推移グラフ</p>
     </header>
   )
 }
