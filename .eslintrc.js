00ee2bd88a28391b44e5fcd3dbfca403941ab3d9
MIYABETaisei <mytspe46@gmail.com>
2023-08-24 16:39:49 +0900

    update: ⚡️ eslintの設定を変更 #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,6 +70,13 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
+    // testing-library
+    'testing-library/no-await-sync-events': [
+      'error',
+      {
+        eventModules: ['fire-event'],
+      },
+    ],
   },
   overrides: [
     {
