bb1d47add60ec287654b45e5ecba4b3146e1dd5e
MIYABETaisei <mytspe46@gmail.com>
2023-08-25 08:52:54 +0900

    test: 🧪 待機処理削除 #1

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,14 +4,12 @@ import {expect, test} from '@playwright/test'
 
 test('titleタグは設定されているか', async ({page}) => {
   await page.goto('http://127.0.0.1:3000/')
-  await page.waitForLoadState('load')
 
   await expect(page).toHaveTitle('株式会社ゆめみのフロントエンド技術課題')
 })
 
 test('都道府県を選択して総人口推移グラフを表示', async ({page}) => {
   await page.goto('http://127.0.0.1:3000/')
-  await page.waitForLoadState('load')
 
   const prefButton = await page.getByRole('button', {name: '北海道'})
   await prefButton.click()
