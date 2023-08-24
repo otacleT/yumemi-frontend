71fe70b757d8c8a80b440b35c917e3fc19d9ffc0
MIYABETaisei <mytspe46@gmail.com>
2023-08-24 22:59:30 +0900

    test: 🧪 アプリの状態、選択するとチャート表示を確認するe2eテスト追加 #1

---
+++ e2e/home.spec.ts
@@ -0,0 +1,18 @@
+/* eslint-disable testing-library/no-await-sync-queries */
+/* eslint-disable testing-library/prefer-screen-queries */
+import {expect, test} from '@playwright/test'
+
+test('titleタグは設定されているか', async ({page}) => {
+  await page.goto('http://127.0.0.1:3000/')
+
+  await expect(page).toHaveTitle('株式会社ゆめみのフロントエンド技術課題')
+})
+
+test('都道府県を選択して総人口推移グラフを表示', async ({page}) => {
+  await page.goto('http://127.0.0.1:3000/')
+
+  const prefButton = await page.getByRole('button', {name: '北海道'})
+  await prefButton.click()
+
+  await expect(page.getByText('出典：RESAS（地域経済分析システム）')).toBeVisible()
+})
